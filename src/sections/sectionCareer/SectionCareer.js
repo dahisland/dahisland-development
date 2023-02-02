857f3e75e87ecb0f4452cd93f887766a1ed16323
Myriam Mornet <dahisland@gmail.com>
2023-02-03 00:23:48 +0100

    Changer order elements in section career

--- src/sections/sectionCareer/SectionCareer.js
+++ src/sections/sectionCareer/SectionCareer.js
@@ -8,14 +8,14 @@ const SectionCareer = React.forwardRef(({ data }, careerRef) => {
     <section id="sectionCareer" ref={careerRef}>
       <div className="section_content">
         <h1>{data.title}</h1>
+        <div className={"sectionCareer_elements"}>
+          <SectionCareerJobs data={data} />
+          <SliderOldProjects data={data} />
+        </div>
         <div className="sectionCareer_formation">
           {data.formation.map((item, index) => (
             <p key={"formation-item-" + index}>{item.toUpperCase()}</p>
           ))}
-        </div>
-        <div className={"sectionCareer_elements"}>
-          <SectionCareerJobs data={data} />
-          <SliderOldProjects data={data} />
         </div>
       </div>
     </section>
